de43baa448634e9346fc50a9b1c6acab2b0034f8
Moose Davis <moosedavis2011@gmail.com>
2020-09-14 23:03:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,6 +7,7 @@ def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
+
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
@@ -14,7 +15,6 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 shipping_manifest["oil paintings"]
 end
 
@@ -27,13 +27,10 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
-
   # add 4 gun powder to the shipping_manifest hash below
-
-
-
   # return the shipping_manifest hash below
+
+shipping_manifest["muskets"] = 2 
+shipping_manifest["gun powder"] = 4
 
 end
