4e897340063ef5feb7dc093b092ddd703fd96258
Moose Davis <moosedavis2011@gmail.com>
2020-09-14 22:59:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,16 +1,10 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  your_hash = {"my_name" => "Moose Davis"}
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
@@ -20,9 +14,13 @@ def retrieval
                   "oil paintings" => 3
                  }
 
+
+retrieval["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
+
+
 
 def adding
   shipping_manifest = {
