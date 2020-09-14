e615ee6ec6e6b5a5dc30a9148dd56878224e2d8f
Moose Davis <moosedavis2011@gmail.com>
2020-09-14 23:04:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,6 +30,7 @@ def adding
   # add 4 gun powder to the shipping_manifest hash below
   # return the shipping_manifest hash below
 
-shipping_manifest["muskets"] = 2, ["gun powder"] = 4
+shipping_manifest["muskets"] = 2 
+shipping_manifest["gun powder"] = 4
 
 end
